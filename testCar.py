395827fbf747570593a590be3aa0ca58e3df849b
Bhea <bheaisipnobleza@gmail.com>
2023-07-01 14:06:33 +0800

    calling brake method five times

--- testCar.py
+++ testCar.py
@@ -3,8 +3,14 @@ from CarClass import Car
 
 # Create an car object
 car = Car(2012, "Ford")
+print("\nAccelerating....\n")
 # Calling accelerate method 5 times
-print("Accelerating....")
 for i in range(5):
     car.accelerate()
     print("The current speed is :", car.get_speed())
+
+print("\nBraking....\n")
+# Calling brake method five times
+for i in range(5):
+    car.brake()
+    print("The current speed is: ", car.get_speed())
