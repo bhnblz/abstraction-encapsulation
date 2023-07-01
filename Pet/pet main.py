405b34739227a6a42efa7948d9739533fe8a4d2c
Bhea <bheaisipnobleza@gmail.com>
2023-07-01 23:40:29 +0800

    Animal type

--- Pet/pet main.py
+++ Pet/pet main.py
@@ -6,3 +6,6 @@ pi = PetInterface()
 
 pet.set_name(pi.name())
 print("Pet's Name: ", pet.get_name())
+
+pet.set_animal_type(pi.animal_type())
+print("Animal Type: ", pet.get_animal_type())
--- Pet/petinterface.py
+++ Pet/petinterface.py
@@ -2,3 +2,6 @@ class PetInterface:
     def name(self):
         name = input("Enter pet's name: ")
         return name
+    def animal_type(self):
+        animal_type = input("Enter the animal type: ")
+        return animal_type
