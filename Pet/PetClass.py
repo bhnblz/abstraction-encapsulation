59ce305599ad8b20eef1a436ff34a5e08377383f
Bhea <bheaisipnobleza@gmail.com>
2023-07-01 14:55:43 +0800

    pseudocode

---
+++ Pet/PetClass.py
@@ -0,0 +1,8 @@
+# Create a class named Pet with the following attributes: name, animal_type, age
+# __init__ method
+# name method
+# get_name method
+# animal type method
+# get_animal_type method
+# age method
+# get_age method
