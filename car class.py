05d4a1656c1860ff939daa87600f89ee37ecdf37
Bhea <bheaisipnobleza@gmail.com>
2023-07-01 12:42:18 +0800

    accelerate method that add 5 in the speed attributes each time it is being called

--- car class.py
+++ car class.py
@@ -1,5 +1,14 @@
 # Create a car class with a attributes of year_model, make and speed
+class Car:
 # __init__ method that accepts car's year model and make as an argument
-# accelarate method
+    def __init__(self, year_model, make):
+        # private members
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+# accelarate method that add 5 to the speed attributes each time it is called
+    def accelerate(self):
+        self.__speed += 5
+
 # brake method
 # get speed method
