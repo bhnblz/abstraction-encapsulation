45c2a100b17302fedf5342b59d9b681919e34b4a
Bhea <bheaisipnobleza@gmail.com>
2023-07-01 12:32:05 +0800

    pseudocode

---
+++ car class.py
@@ -0,0 +1,5 @@
+# Create a car class with a attributes of year_model, make and speed
+# __init__ method that accepts car's year model and make as an argument
+# accelarate method
+# brake method
+# get speed method
