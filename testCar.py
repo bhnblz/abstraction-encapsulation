1200b6c6bda9f00686d31df94ad57ddb6441477d
Bhea <bheaisipnobleza@gmail.com>
2023-07-01 13:58:11 +0800

    calling accelerate method 5 times

--- testCar.py
+++ testCar.py
@@ -3,7 +3,7 @@ from CarClass import Car
 
 # Create an car object
 car = Car(2012, "Ford")
-
+# Calling accelerate method 5 times
 print("Accelerating....")
 for i in range(5):
     car.accelerate()
