7ee91c4b853f2a44dd70140331104e7cb353dee7
Bhea <bheaisipnobleza@gmail.com>
2023-07-02 06:04:59 +0800

    pseudocode

---
+++ fan/fanclass.py
@@ -0,0 +1,7 @@
+# Create a class named Fan
+# Input three constant named SLOW, MEDIUM, AND FAST with a values of 1,2, and 3to denote the speed of the fan
+# Private int data field named speed
+# Private bool data field named on that specifies the speed of the fan
+# Private float data field named radius that specifies the radius of the fan
+# Private string data field named color that specifies the color of the fan 
+# constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
