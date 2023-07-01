0f0f695fdfcdb642e42251dd418a33199cb586bc
Bhea <bheaisipnobleza@gmail.com>
2023-07-02 07:40:40 +0800

    fan 2 properties

--- fan/testFan.py
+++ fan/testFan.py
@@ -2,17 +2,25 @@
 from fanclass import Fan
 # Create test fan class
 class TestFan:
-    def main(self):
-        # Fan 1 objects
-        fan1 = Fan(Fan.FAST, 10, 'yellow', True)
+    def fan(self):
+        # Fan 1 object
+        fan_1 = Fan(Fan.FAST, 10, "yellow", on=True)
 
-        print("Fan 1 Properties: ")
-        print("Speed:", fan1.get_speed())
-        print("Radius:", fan1.get_radius())
-        print("Color:", fan1.get_color())
-        print("On [on = True, off = False]:", fan1.fan_is_on())
+        print("\n\033[31mFAN 1 PROPERTIES: ")
+        print("\033[35mSpeed:", fan_1.get_speed())
+        print("\033[34mRadius:", fan_1.get_radius())
+        print("\033[33mColor:", fan_1.get_color())
+        print("\033[32mOn [on = True, off = False]:", fan_1.fan_is_on())
     
+        # Fan 2 object
+        fan_2 = Fan(Fan.MEDIUM, 5, "blue", on=False)
 
+        print("\n\033[31mFAN 2 PROPERTIES: ")
+        print("\033[32mSpeed:", fan_2.get_speed())
+        print("\033[33mRadius:", fan_2.get_radius())
+        print("\033[34mColor:", fan_2.get_color())
+        print("\033[35mOn [on = True, off = False]:", fan_2.fan_is_on())
+    
 # Create Instance of the TestFan to run
 test_fan = TestFan()
-test_fan.main()
+test_fan.fan()
